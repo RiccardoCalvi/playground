2b61b8f5535bfeb60a44e28e298cddc851b52003
Riccardo Calvi <riccardoclv@gmail.com>
2025-04-24 23:02:39 +0200

    Amenities => Aggiunto componente

--- ladimora/index.js
+++ ladimora/index.js
@@ -4,3 +4,4 @@ import './src/styles/main.scss';
 import './src/components/card/card';
 import './src/components/rules/rules';
 import './src/components/nearby/nearby';
+import './src/components/amenities/amenities';
